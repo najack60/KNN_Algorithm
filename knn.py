419415cecc55d7756cede5ed68b408e8ff14330d
najack60 <35020264+najack60@users.noreply.github.com>
2021-09-25 20:44:44 -0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Nate Colbert
+# FILENAME: knn.py
+# SPECIFICATION: implements the knn algorithm
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 3-5 hours
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
